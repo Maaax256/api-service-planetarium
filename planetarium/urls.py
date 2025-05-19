250d8c3be27af0425a501767cbbcf5d48d104ddb
Maksym Omelkov <omelkov85@gmail.com>
2025-05-19 04:24:29 +0300

    added urls for models

---
+++ planetarium/urls.py
@@ -0,0 +1,48 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from planetarium.views import (
+    ShowThemeViewSet,
+    AstronomyShowViewSet,
+    PlanetariumDomeViewSet,
+    ReservationViewSet,
+    ShowSessionViewSet,
+    TicketViewSet,
+)
+
+app_name = "planetarium"
+
+router = DefaultRouter()
+router.register(
+    "show-themes",
+    ShowThemeViewSet,
+    basename="show-theme"
+)
+router.register(
+    "astronomy-shows",
+    AstronomyShowViewSet,
+    basename="astronomy-show"
+)
+router.register(
+    "planetarium-domes",
+    PlanetariumDomeViewSet,
+    basename="planetarium-dome"
+)
+router.register(
+    "reservations",
+    ReservationViewSet,
+    basename="reservation"
+)
+router.register(
+    "show-sessions",
+    ShowSessionViewSet,
+    basename="show-session"
+)
+router.register(
+    "tickets",
+    TicketViewSet,
+    basename="ticket"
+)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
